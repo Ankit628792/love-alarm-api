4ea5c339abb0c126d9659cefe948c05f39f6b759
Ankit Kumar <ankit@sugoilabs.com>
2023-08-28 12:37:54 +0530

    2.0.9

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -6,9 +6,10 @@ const OTPs = require("../models/otp.model");
 const Plans = require("../models/plan.model");
 const Users = require("../models/user.model");
 const countryToCurrency = require('country-to-currency');
-const countryCodes = require('../../CountryCodes.json')
+const countryCodes = require('../../CountryCodes.json');
+const { sendEmail } = require("../../helpers/sendEmail");
 
-const sendOtp = async (req, res) => {
+const sendOtpOld = async (req, res) => {
     try {
         if (req.body.mobile) {
             let otp = generateOTP();
@@ -47,12 +48,81 @@ const sendOtp = async (req, res) => {
     }
 }
 
+const sendOtp = async (req, res) => {
+    try {
+        let email = req.body.email.replace(/\s+/g, '');
+        let mobile = req.body.mobile.replace(/\s+/g, '')
+        if (mobile && email) {
+            email = email?.toLowerCase()?.trim();
+
+            let user = await Users.findOne({ $or: [{ mobile }, { email }] }).lean()
+
+            if (user) {
+                let msg;
+                if (user.mobile == mobile && user.email != email) {
+                    msg = "Email is not associated with the provided mobile number"
+                }
+                else if (user.mobile != mobile && user.email == email) {
+                    msg = "Mobile number is not associated with the provided email"
+                }
+                if (msg) {
+                    return res.status(200).send({
+                        success: false,
+                        message: msg
+                    })
+                }
+            }
+
+            let otp = generateOTP();
+            var message = `
+            <p style="font-size: 16px">Dear User, Your OTP to authenticate login on Love Alarm 2.0 is <span style="font-size: 17px; font-weight: 600">${otp}</span>, valid for next 5 minutes. Do not share this OTP with anyone.</p> 
+            <p style="font-size: 16px">To know more about the app, visit <a href="www.lovealarm.in">www.lovealarm.in</a></p>
+            <p style="font-size: 18px">Regards,<br/><strong>Love Alarm 2.0</strong></p>
+            `
+
+            let data = {
+                email: email,
+                subject: `OTP for Love Alarm 2.0 login`,
+                text: `${message}`,
+                html: `${message}`
+            }
+
+            await sendEmail(data)
+
+            console.log(otp)
+            let doc = await OTPs.create({
+                mobile: mobile.replace(/\s+/g, ''),
+                email: email.replace(/\s+/g, ''),
+                otp: encrypt(otp?.toString(), otp?.toString())
+            })
+            setTimeout(() => OTPs.findByIdAndUpdate({ _id: doc._id }, { isExpired: true }), 1000 * 60 * 5);
+            res.status(201).send({
+                success: true,
+                message: 'OTP sent successfully!',
+                otp: otp
+            })
+        }
+        else {
+            res.status(400).send({
+                success: false,
+                message: 'Missing Params'
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({
+            success: false,
+            message: error?.message
+        })
+    }
+}
+
 const verifyOtp = async (req, res) => {
     try {
-        let { mobile, otp, fcmToken, countryCode } = req.body
-        let country = countryCodes.find(item => item.iso == countryCode).country
-        if (mobile && otp) {
-            let isOtp = await OTPs.findOne({ mobile: mobile.replace(/\s+/g, ''), otp }).sort({ createdAt: -1 });
+        let { mobile, otp, fcmToken, countryCode, email } = req.body
+        if (mobile && otp && email) {
+            email = email?.toLowerCase().trim()
+            let isOtp = await OTPs.findOne({ mobile: mobile.replace(/\s+/g, ''), email: email.replace(/\s+/g, ''), otp }).sort({ createdAt: -1 });
             if (isOtp || envs.MAGIC_OTP == otp) {
                 if (isOtp?.isExpired && envs.MAGIC_OTP !== otp) {
                     res.status(200).send({
@@ -62,7 +132,7 @@ const verifyOtp = async (req, res) => {
                 }
                 else {
                     let plan = await Plans.findOne({ planType: 'subscription', amount: 10 })
-                    let isUser = await Users.findOne({ mobile: mobile.replace(/\s+/g, '') }).lean()
+                    let isUser = await Users.findOne({ mobile: mobile.replace(/\s+/g, ''), email: email.replace(/\s+/g, '') }).lean()
                     let user;
 
                     if (isUser) {
@@ -79,6 +149,7 @@ const verifyOtp = async (req, res) => {
                         user = await Users.create({
                             plan: plan._id,
                             mobile: mobile.replace(/\s+/g, ''),
+                            email: email.replace(/\s+/g, ''),
                             heartId: generateHeartId(7),
                             fcmToken,
                             referralCode: generateReferralCode(),
--- src/models/otp.model.js
+++ src/models/otp.model.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
-const { AES } = require('crypto-js');
 
 var schema = new mongoose.Schema({
     mobile: { type: String, trim: true, required: true },
+    email: { type: String, trim: true, required: true },
     otp: { type: String, trim: true, required: true },
     isExpired: { type: Boolean, default: false }
 }, { timestamps: true });
@@ -10,25 +10,3 @@ var schema = new mongoose.Schema({
 const OTPs = mongoose.model('OTPs', schema);
 
 module.exports = OTPs;
-
-
-// Pre-save middleware
-// schema.pre('save', async function (next) {
-
-//     // Encrypt the sensitive data if it is being modified or is new
-//     if (this.isModified('otp') || this.isNew) {
-//         try {
-//             this.otp = AES.encrypt(this.otp, this.otp).toString();
-//         } catch (error) {
-//             return next(error);
-//         }
-//     }
-//     if (this.isModified('mobile') || this.isNew) {
-//         try {
-//             this.mobile = AES.encrypt(this.mobile, this.mobile).toString();
-//         } catch (error) {
-//             return next(error);
-//         }
-//     }
-//     next();
-// });
